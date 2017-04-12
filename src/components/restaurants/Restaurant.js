a749ed8350e6598f0246275842c3f4b6a59764a6
Devin Rieger <dev0089@gmail.com>
2017-04-12 16:21:58 -0400

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput.js'
 
 class Restaurant extends Component {
+  handleClick() {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    })
+  }
   render() {
     return (
       <div>
+        <li>
+          {this.props.restaurant.text}
+          <button onClick={() => this.handleClick()}>Delete</button>
+          <ReviewInput store={this.props.store} restaurantId={this.props.restaurant.id}/>
+        </li>
       </div>
     );
   }
--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  constructor() {
+    super()
+    this.state = {text: ''}
+  }
+  handleInput(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      text: this.state.text
+    })
+  }
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={event => this.handleSubmit(event)}>
+          New Restaurant:
+          <input onChange={event => this.handleInput(event)}/>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
