5e6e1676bf275fb287a1550c3f0f0f3dd58698e3
fakebenjay <fakebenjay@gmail.com>
2017-04-13 11:53:59 -0400

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -6,22 +6,28 @@ export default function manageRestaurants(state = {
   reviews: []
 }, action) {
   switch (action.type) {
+
     case 'ADD_RESTAURANT':
       let restaurant = { text: action.restaurant.text, id: cuidFn() }
       return Object.assign({}, state, {
         restaurants: state.restaurants.concat(restaurant)
       })
+
     case 'DELETE_RESTAURANT':
       let restaurants = state.restaurants.filter(rest => rest.id !== action.id)
-      return Object.assign({}, state, { restaurants })
+      let revs = state.reviews.filter(rev => rev.restaurantId !== action.id)
+      return Object.assign({}, state, { restaurants, reviews:revs })
+
     case 'ADD_REVIEW':
       let review = Object.assign({}, action.review, {id: cuidFn()} )
       return Object.assign({}, state, {
         reviews: state.reviews.concat(review)
       })
+
     case 'DELETE_REVIEW':
       let reviews = state.reviews.filter(review => review.id !== action.id)
       return Object.assign({}, state, { reviews })
+
     default:
       return state;
   }
