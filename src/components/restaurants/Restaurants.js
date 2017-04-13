c20353a01b3882a670d07a0d283ca0199a84584f
github username <github email address>
2017-04-13 09:48:17 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
-  render() {
-    return(
-      <ul>
-        Restaurants Component
-      </ul>
-    );
-  }
-};
+
+   render() {
+     const { store } = this.props
+     const restaurants = store.getState().restaurants.map((restaurant, index) => {
+       return <Restaurant key={index} restaurant={restaurant} store={store} />
+    })
+
+     return (
+       <ul>
+         {restaurants}
+       </ul>
+     )
+   }
+ }
 
 export default Restaurants;
