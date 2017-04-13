46ac7858176f009a96b91b07f47e0cc97685cf72
gee2789 <geeyung.li@gmail.com>
2017-04-12 22:22:55 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,21 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const reviews = this.props.store.getState().reviews
+    const validReviews = reviews.filter((review)=> review.restaurantId === this.props.restaurantId)
+
+    let filteredReviews = validReviews.map((review, index)=>{
+      return  <Review
+                key={index}
+                review={review}
+                restaurantId={this.props.restaurantId}
+                store={this.props.store}
+                />
+    })
+
     return (
       <ul>
-        Reviews
+        {filteredReviews}
       </ul>
     );
   }
